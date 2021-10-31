6f500e1555d671548d2216aea3769ec26f52766e
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-30 21:05:28 -0300

    aula01 and aula02

---
+++ Aula01_commandosBasicos.bash
@@ -0,0 +1,43 @@
+#Linux
+
+#diretorio atual
+$pwd
+
+#visualizar arquivos
+$dir
+#ou
+$ls
+$ls documentos
+$ls downloads
+
+#criar diretório
+$mkdir nova_pasta
+
+#visualizar aquivos com detalhes/caracteristicas
+$ls -l
+
+#comando utilizados durante a execução do terminal
+$history
+
+#executar último comando
+$!!
+
+#Comando MV
+#Duas principais funções
+#Renomear
+$mv nova_pasta Linux
+#Mover
+$mv Linux ~
+$ls ~ (move a pasta para onde digitar esse comando)
+
+#Cria documentos vazios
+$touch teste.txt
+
+#Mudando o nome do arquivos
+$mv teste.txt linux.txt
+
+#copiando arquivos
+$cp linux.txt home/alladio
+
+#remover arquivos
+$rm linux.txt
---
+++ aula02_lidandoComArquivos.bash
@@ -0,0 +1,11 @@
+#Arquivos.txt
+
+$nano
+#Ctrl+G -> Obter ajuda
+#Ctrl+O -> Gravar
+#Ctrl+G ->
+#Ctrl+G ->
+#Ctrl+G ->
+#Ctrl+G ->
+#Ctrl+G ->
+
