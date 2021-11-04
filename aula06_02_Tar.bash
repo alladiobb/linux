58bf474f542995448e120cb95cf670e327f4b4d2
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-11-04 18:29:34 -0300

    Update aula06_02_Tar.bash

--- aula06_02_Tar.bash
+++ aula06_02_Tar.bash
@@ -3,8 +3,8 @@
 $tar -cf arquivos.tar aularedes.txt arquivo.txt teste.txt
 $gzip arquivos.tar]
 
+#Extrair
 $tar -xvf arquivos.txt.tar.gz
-
 
 #Extrair em alguma pasta
 $tar -xvf arquivos.txt.tar.gz -C ~/Documentos/extrair/
