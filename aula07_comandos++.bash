2863b81ca55416a2f74aaf3098f1bc8adc6e059e
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-11-04 18:45:47 -0300

    Aulas Linux 07 e 08

---
+++ aula07_comandos++.bash
@@ -0,0 +1,24 @@
+#Commandos++
+
+#número de linhas
+$nl teste.txt
+$wc -l teste.txt
+
+#número de palavras
+$wc -w teste.txt
+
+#número de caracteries
+$wc -m teste.txt
+
+#Arquivos e diretórios ocultos
+$ls -a
+
+#Diretórios com / no fim
+$ls -F
+
+#comparar dois arquivos
+$cmp aularedes.txt teste.txt
+$diff aularedes.txt teste.txt
+
+#organizar em ordem numérica
+$sort -n teste.txt
---
+++ aula08_LastRebootRouteTtimeCowsayCmatrix.bash
@@ -0,0 +1,23 @@
+#LAST REBOOT, ROUTE, TIME, COWSAY e CMATRIX
+
+#Últimas reinicializações
+$last reboot
+
+#Mostra o roteamento da maquina
+$route -n
+
+#calcula o tempo do comando
+$time traceroute
+
+#Tempo que o sistema está rondado
+$uptime
+
+
+#cowsay
+$sudo apt install cowsay
+
+$cowsay "Linux é legal!"
+
+#CMATRIX
+$sudo apt install cmatrix
+
