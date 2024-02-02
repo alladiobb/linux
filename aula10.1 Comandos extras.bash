afd9b9e80572141c4feddf451b5f28d4a9183f2d
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-02-01 22:58:49 -0300

    Add: New commands (create variabels) and Print uppercase words Refact: Add comments

--- aula10.1 Comandos extras.bash
+++ aula10.1 Comandos extras.bash
@@ -1,7 +1,10 @@
+#Coloca dentro da variável "V" == "Palavra" 
+V=palavra
 
+#Imprimir todas as letras em maiusculo
+echo ${V^^}
 
+#Diferença do tempo de execução de três maneiras de colocar as palavras em maiusculo  
 time for ((i=1; i<2000; i++)); { tr a-z A-Z <<< $V > /dev/null; }
-
 time for ((i=1; i<2000; i++)); { echo ${V^^} > /dev/null; }
-
 time for ((i=1; i<2000; i++)); { : ${V^^} > /dev/null; }
